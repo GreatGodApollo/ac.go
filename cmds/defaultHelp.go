5887e4c2b2b260be0603562ee71aa9cb4411c74e
Brett Bender <brett@brettb.xyz>
2020-04-10 16:56:26 -0500

    fix: defaultHelp

--- cmds/defaultHelp.go
+++ cmds/defaultHelp.go
@@ -1,19 +1,20 @@
 package cmds
 
-func DefaultHelp() *Command {
-	return &Command{
-		Name:        "help",
-		Description: "Get some help with commands!",
-		OwnerOnly:   false,
-		Hidden:      false,
-		Type:        Everywhere,
-		Execute:     helpExecute,
-		SubOnly:     false,
-		UserPerms:   0,
-		BotPerms:    0,
-	}
+import "github.com/GreatGodApollo/acgo/permissions"
+
+var DefaultHelp = &Command{
+	Name:        "help",
+	Description: "Get some help with commands!",
+	OwnerOnly:   false,
+	Hidden:      false,
+	Type:        Everywhere,
+	Execute:     helpExecute,
+	SubOnly:     false,
+	UserPerms:   0,
+	BotPerms:    permissions.PermissionMessagesSend,
 }
 
 func helpExecute(ctx *Context) error {
-
+	_, err := ctx.Reply("WIP")
+	return err
 }
